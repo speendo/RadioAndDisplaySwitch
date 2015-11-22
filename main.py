d8009ccba2700a73f043735242909e518f54381d
Marcel Jira <m2m@gmx.at>
2015-11-22 13:43:12 +0100

    corrected for newer version

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 __author__ = 'marcel'
 
 from mpd import MPDClient, MPDError
-from lib.Photocell.lib.Photocell import CheckLight
-from lib.FlushFMLCD.lib.LCDController import LCD
-from lib.FlushFMLCD.lib.LineController import TimeLine, MPDLine, FetchLine
+from lib.photocell.photocell import CheckLight
+from lib.mpd_lcd.lcd_controller import LCD
+from lib.mpd_lcd.line_controller import TimeLine, MPDLine, FetchLine
 
 # MPD Client
 client = MPDClient()
@@ -21,10 +21,10 @@ lcd.set_line("song", MPDLine(lcd, 3, "title"))
 lcd.set_line("proverb", FetchLine(lcd, 4, "http://sprichwortgenerator.de/plugin.php"))
 
 # init
-lcd.lineContainer["time"].run_every()
-lcd.lineContainer["station"].run_every()
-lcd.lineContainer["song"].run_every()
-lcd.lineContainer["proverb"].run_every()
+lcd.line_container["time"].run_every()
+lcd.line_container["station"].run_every()
+lcd.line_container["song"].run_every()
+lcd.line_container["proverb"].run_every()
 
 
 # define methods to run when light state changes
